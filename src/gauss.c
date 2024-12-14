90333d57403b5f97882ec3678c6e972d87ec73b1
PwAAstud <01197780@pw.edu.pl>
2024-12-14 16:42:08 +0100

    imlementacja gustawa

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,31 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+	for(int ir=0; ir < mat->r-1; ir++){
+		if( ir == mat->c){
+			break;
+		}
 
-		return 0;
+		double podzielPrzez = mat->data[ir][ir];
+		for(int ic=0; ic<mat->r; ic++){
+			mat->data[ir][ic] /= podzielPrzez;
+		}
+		for(int ic=0; ic<b->r; ic++){
+			b->data[ir][ic] /= podzielPrzez;
+		}
+
+		for(int iMinC = ir+1; iMinC < mat->r; iMinC++){
+			double mnoznik = mat->data[iMinC][ir];
+			for(int ic=0; ic<mat->r; ic++){
+				mat->data[iMinC][ic] -= mat->data[ir][ic] * mnoznik;
+			}
+			for(int ic=0; ic<b->r; ic++){
+				b->data[iMinC][ic] -= b->data[ir][ic] * mnoznik;
+			}
+		}
+
+	}
+
+	return 0;
 }
 
