4ea6c1209454f627b0c8ddf07357740cbbe48912
BrisingrRaugr <kamilgomula5@gmail.com>
2024-12-19 03:08:49 +0100

    poprawa wstawiania wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -7,19 +7,31 @@
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 
 				int i, p, j;
+				if( mat->r == x->r && mat->r == mat->c && mat->r == b->r && b->r == x->r)
+				{
 				for (i = x->r - 1; i >= 0; i--) {
 
 								j=i;
 								p=b->data[i][0];
+								if(  mat->data[i][i] != 0)
+								{
 								while(j < x->r -1)
                                 {
                                     p-=x->data[j][0] * mat->data[i][j];
                                     j++;
                                 }
 							x->data[i][0]= p / mat->data[i][i];
+								}
+								else
+								{
+								return 1;
+								}
 				}
 
 				return 0;
+				}
+				else
+				return 2;
 }
 
 
